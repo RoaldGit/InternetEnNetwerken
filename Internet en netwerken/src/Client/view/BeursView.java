7bfd6f0580a5efdb552c7e63e81f74299a2599f2
Roald <305953@student.saxion.nl>
2013-11-07 19:34:13 +0100

    Labels voor tables (lol, rijmt) toegevoegd

--- Internet en netwerken/src/Client/view/BeursView.java
+++ Internet en netwerken/src/Client/view/BeursView.java
@@ -3,6 +3,7 @@ package Client.view;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
@@ -15,62 +16,127 @@ public class BeursView extends JPanel implements Observer {
 			sellingScroll;
 	private UserModel userModel;
 	private String user;
+	private String[] portoHeader, buyingHeader, sellingHeader, buyHeader,
+			sellHeader;
+	private Object[][] dummyPorto, dummyBuying, dummySelling, dummyBuy,
+			dummySell;
+	private JLabel portoLabel, buyingLabel, sellingLabel, buyLabel, sellLabel;
 
 	public BeursView(UserModel uModel) {
 		this.userModel = uModel;
 		this.setLayout(null);
 
-		user = userModel.getUser();
-
-		String[] portoHeader = { "Aandeel", "Aantal", "Aanschaf Prijs",
+		portoHeader = new String[] { "Aandeel", "Aantal", "Aanschaf Prijs",
 				"Totale waarde" };
-		String[] buyingHeader = { "Aandeel", "Aantal", "Geboden Prijs",
+		buyingHeader = new String[] { "Aandeel", "Aantal", "Geboden Prijs",
 				"Totale waarde" };
-		String[] sellingHeader = { "Aandeel", "Aantal", "Vraagprijs",
+		sellingHeader = new String[] { "Aandeel", "Aantal", "Vraagprijs",
 				"Totale waarde" };
-		String[] buyHeader = { "Koper", "Aandeel", "Aantal", "Geboden Prijs",
+		buyHeader = new String[] { "Koper", "Aandeel", "Aantal",
+				"Geboden Prijs",
 				"Totale waarde" };
-		String[] sellHeader = { "Aanbieder", "Aandeel", "Aantal", "Vraagprijs",
+		sellHeader = new String[] { "Aanbieder", "Aandeel", "Aantal",
+				"Vraagprijs",
 				"Totale waarde" };
 
-		Object[][] dummyPorto = { { "Syntaxis", 10, 5.00, 50.00 },
-				{ "Watt", 5, 5.00, 25.00 } };
-		Object[][] dummyBuying = { { "LiNK", 8, 5.00, 40.00 } };
-		Object[][] dummySelling = { { "Syntaxis", 5, 5.10, 25.50 } };
-		Object[][] dummyBuy = { { user, "Syntaxis", 20, 5.00, 100.00 },
-				{ "User", "LiNK", 8, 5.00, 40.00 } };
-		Object[][] dummySell = { { user, "Syntaxis", 5, 5.10, 25.50 } };
+		portoLabel = new JLabel("Uw Portofeuille:");
+		buyingLabel = new JLabel("Uw inkooporders:");
+		sellingLabel = new JLabel("Uw verkooporders:");
+		buyLabel = new JLabel("Inkooporders voor -aandeel-");
+		sellLabel = new JLabel("Verkooporders voor -aandeel-");
 
-		porto = new Tabel(dummyPorto, portoHeader);
-		buying = new Tabel(dummyBuying, buyingHeader);
-		selling = new Tabel(dummySelling, sellingHeader);
-		buy = new Tabel(dummyBuy, buyHeader);
-		sell = new Tabel(dummySell, sellHeader);
+		portoLabel.setBounds(5, 80, 200, 20);
+		buyingLabel.setBounds(5, 230, 200, 20);
+		sellingLabel.setBounds(5, 380, 200, 20);
+		buyLabel.setBounds(375, 80, 200, 20);
+		sellLabel.setBounds(375, 230, 200, 20);
 
-		portoScroll = new JScrollPane(porto);
-		buyingScroll = new JScrollPane(buying);
-		sellingScroll = new JScrollPane(selling);
-		buyScroll = new JScrollPane(buy);
-		sellScroll = new JScrollPane(sell);
+		add(portoLabel);
+		add(buyingLabel);
+		add(sellingLabel);
+		add(buyLabel);
+		add(sellLabel);
 
-		portoScroll.setBounds(5, 100, 350, 100);
-		buyingScroll.setBounds(5, 250, 350, 100);
-		sellingScroll.setBounds(5, 400, 350, 100);
-		buyScroll.setBounds(375, 100, 400, 100);
-		sellScroll.setBounds(375, 250, 400, 100);
-
-		add(portoScroll);
-		add(buyingScroll);
-		add(sellingScroll);
-		add(buyScroll);
-		add(sellScroll);
+		updateTables();
 	}
 
 	public void update(Observable arg0, Object arg1) {
-		buy.updateUI();
+
 	}
 
-	public void update() {
-		buy.updateUI();
+	public void updateTables() {
+		updateTableData();
+
+		updatePorto();
+		updateBuying();
+		updateSelling();
+		updateBuy();
+		updateSell();
+	}
+
+	public void updateTableData() {
+		user = userModel.getUser();
+		dummyPorto = new Object[][] { { "Syntaxis", 10, 5.00, 50.00 },
+				{ "Watt", 5, 5.00, 25.00 } };
+		dummyBuying = new Object[][] { { "LiNK", 8, 5.00, 40.00 } };
+		dummySelling = new Object[][] { { "Syntaxis", 5, 5.10, 25.50 } };
+		dummyBuy = new Object[][] { { user, "Syntaxis", 20, 5.00, 100.00 },
+				{ "User", "LiNK", 8, 5.00, 40.00 } };
+		dummySell = new Object[][] { { user, "Syntaxis", 5, 5.10, 25.50 } };
+	}
+
+	public void updatePorto() {
+		if (porto != null)
+			remove(portoScroll);
+
+		porto = new Tabel(dummyPorto, portoHeader);
+		portoScroll = new JScrollPane(porto);
+		portoScroll.setBounds(5, 100, 350, 100);
+
+		add(portoScroll);
+	}
+
+	public void updateBuying() {
+		if (buying != null)
+			remove(buyingScroll);
+
+		buying = new Tabel(dummyBuying, buyingHeader);
+		buyingScroll = new JScrollPane(buying);
+		buyingScroll.setBounds(5, 250, 350, 100);
+
+		add(buyingScroll);
+	}
+
+	public void updateSelling() {
+		if (selling != null)
+			remove(sellingScroll);
+
+		selling = new Tabel(dummySelling, sellingHeader);
+		sellingScroll = new JScrollPane(selling);
+		sellingScroll.setBounds(5, 400, 350, 100);
+
+		add(sellingScroll);
+	}
+
+	public void updateBuy() {
+		if (buy != null)
+			remove(buyScroll);
+
+		buy = new Tabel(dummyBuy, buyHeader);
+		buyScroll = new JScrollPane(buy);
+		buyScroll.setBounds(375, 100, 400, 100);
+
+		add(buyScroll);
+	}
+
+	public void updateSell() {
+		if (sell != null)
+			remove(sellScroll);
+
+		sell = new Tabel(dummySell, sellHeader);
+		sellScroll = new JScrollPane(sell);
+		sellScroll.setBounds(375, 250, 400, 100);
+
+		add(sellScroll);
 	}
 }
