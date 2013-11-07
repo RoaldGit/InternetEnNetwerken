cbe85f637d2ce5da4ac28501733b9835c1c0129a
Roald <305953@student.saxion.nl>
2013-11-07 13:16:52 +0100

    Table vervangen met tabel

--- Internet en netwerken/src/Client/view/BeursView.java
+++ Internet en netwerken/src/Client/view/BeursView.java
@@ -4,10 +4,11 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
+import Client.control.Tabel;
 import Client.model.UserModel;
 
 public class BeursView extends JPanel {
-	private JTable porto, buy, sell, buying, selling;
+	private Tabel porto, buy, sell, buying, selling;
 	private JScrollPane portoScroll, buyScroll, sellScroll, buyingScroll,
 			sellingScroll;
 	private UserModel userModel;
@@ -36,11 +37,11 @@ public class BeursView extends JPanel {
 				{ "User", "LiNK", 8, 5.00, 40.00 } };
 		Object[][] dummySell = { { "User", "Syntaxis", 5, 5.10, 25.50 } };
 
-		porto = new JTable(dummyPorto, portoHeader);
-		buying = new JTable(dummyBuying, buyingHeader);
-		selling = new JTable(dummySelling, sellingHeader);
-		buy = new JTable(dummyBuy, buyHeader);
-		sell = new JTable(dummySell, sellHeader);
+		porto = new Tabel(dummyPorto, portoHeader);
+		buying = new Tabel(dummyBuying, buyingHeader);
+		selling = new Tabel(dummySelling, sellingHeader);
+		buy = new Tabel(dummyBuy, buyHeader);
+		sell = new Tabel(dummySell, sellHeader);
 
 		portoScroll = new JScrollPane(porto);
 		buyingScroll = new JScrollPane(buying);
@@ -48,11 +49,11 @@ public class BeursView extends JPanel {
 		buyScroll = new JScrollPane(buy);
 		sellScroll = new JScrollPane(sell);
 
-		portoScroll.setBounds(5, 100, 300, 100);
-		buyingScroll.setBounds(5, 250, 300, 100);
-		sellingScroll.setBounds(5, 400, 300, 100);
-		buyScroll.setBounds(325, 10, 300, 100);
-		sellScroll.setBounds(325, 125, 300, 100);
+		portoScroll.setBounds(5, 100, 350, 100);
+		buyingScroll.setBounds(5, 250, 350, 100);
+		sellingScroll.setBounds(5, 400, 350, 100);
+		buyScroll.setBounds(375, 10, 350, 100);
+		sellScroll.setBounds(375, 125, 350, 100);
 
 		add(portoScroll);
 		add(buyingScroll);
