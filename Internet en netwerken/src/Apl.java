e764c0775b243c43fd0eda13e8fce4802c1f2185
Roald <305953@student.saxion.nl>
2013-11-08 15:36:24 +0100

    Server test

--- Internet en netwerken/src/Apl.java
+++ Internet en netwerken/src/Apl.java
@@ -1,20 +1,21 @@
-import server.NanoHTTPDchild;
+import server.WebServer;
 import Client.view.MainView;
 
 public class Apl {
-	private static boolean serverTest = false;
+	private static boolean serverTest = true;
 	private static boolean clientTest = true;
 	private static boolean databaseTest = false;
 
 	public static void main(String args[]) {
 		if (serverTest) {
-			NanoHTTPDchild server = new NanoHTTPDchild(8080);
-			try {
-				server.start();
-			} catch (Exception e) {
-				System.out.println(e);
-				System.out.println("Server boot");
-			}
+			// NanoHTTPDchild server = new NanoHTTPDchild(8080);
+			// try {
+			// server.start();
+			// } catch (Exception e) {
+			// System.out.println(e);
+			// System.out.println("Server boot");
+			// }
+			WebServer server = new WebServer(800);
 		}
 
 		if (clientTest) {
