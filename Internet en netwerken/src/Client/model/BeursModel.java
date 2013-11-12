c091dc48bac1a121c03ef94de4ca46c2739dc0b7
Roald <305953@student.saxion.nl>
2013-11-12 14:54:01 +0100

    Model aangepast om changes door te geven aan view

--- Internet en netwerken/src/Client/model/BeursModel.java
+++ Internet en netwerken/src/Client/model/BeursModel.java
@@ -2,9 +2,36 @@ package Client.model;
 
 import java.util.Observable;
 
+import Client.control.Tabel;
+
 public class BeursModel extends Observable {
+	private String aandeelSelect;
+	private Tabel tabelSelect = null;
 
 	public BeursModel() {
 
+	}
+
+	public void setSelectedAandeel(String select) {
+		aandeelSelect = select;
+		System.out.println("BeursModel|setSelected: " + select);
+		setChanged();
+		notifyObservers("aandeelSelect");
+	}
+	
+	public void setSelectedTable(Tabel select) {
+		Tabel oldSelect = null;
+
+		if (select != tabelSelect)
+			oldSelect = tabelSelect;
+		
+		tabelSelect = select;
+		setChanged();
+
+		notifyObservers(oldSelect);
+	}
+
+	public String getSelectedAandeel() {
+		return aandeelSelect;
 	}
 }
