47e5948d9b772f9d704bd38cb7fae5b3bff192f6
Roald <305953@student.saxion.nl>
2013-11-12 14:53:26 +0100

    Action listener toegevoegd

--- Internet en netwerken/src/Client/control/AandelenLijst.java
+++ Internet en netwerken/src/Client/control/AandelenLijst.java
@@ -1,9 +1,26 @@
 package Client.control;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JComboBox;
 
+import Client.model.BeursModel;
+
 public class AandelenLijst extends JComboBox {
-	public AandelenLijst(String[] aandelen) {
+	private BeursModel beursModel;
+
+	public AandelenLijst(String[] aandelen, BeursModel bModel) {
 		super(aandelen);
+		this.beursModel = bModel;
+		beursModel.setSelectedAandeel(this.getSelectedItem().toString());
+		this.addActionListener(new ComboListener());
+	}
+
+	class ComboListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			JComboBox combo = (JComboBox) e.getSource();
+			beursModel.setSelectedAandeel(combo.getSelectedItem().toString());
+		}
 	}
 }
