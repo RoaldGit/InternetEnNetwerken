412c7dbb52bd0372571dc463e14a68e1eb9f7903
Roald <305953@student.saxion.nl>
2013-11-12 14:53:44 +0100

    Deselect toegevoegd

--- Internet en netwerken/src/Client/control/Tabel.java
+++ Internet en netwerken/src/Client/control/Tabel.java
@@ -3,25 +3,31 @@ package Client.control;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.text.NumberFormat;
-import java.util.Locale;
 
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableColumnModel;
 
+import Client.model.BeursModel;
+
 public class Tabel extends JTable {
+	private BeursModel beursModel;
+
 	public Tabel() {
 		super();
 
 		addMouseListener(new ClickEvent());
 	}
 
-	public Tabel(Object[][] row, String[] col) {
+	public Tabel(Object[][] row, String[] col, BeursModel bModel) {
 		super(row, col);
+
 		int cols = col.length - 1;
 		TableColumnModel tcm = this.getColumnModel();
 		tcm.getColumn(cols - 1).setCellRenderer(new NumberRenderer());
 		tcm.getColumn(cols).setCellRenderer(new NumberRenderer());
+
+		beursModel = bModel;
 
 		addMouseListener(new ClickEvent());
 	}
@@ -44,13 +50,15 @@ public class Tabel extends JTable {
 
 	class ClickEvent extends MouseAdapter {
 		public void mouseClicked(MouseEvent e) {
-			JTable source = (JTable) e.getSource();
+			Tabel source = (Tabel) e.getSource();
 			int row = source.getSelectedRow();
 			int cols = source.getColumnCount();
 
 			for (int i = 0; i < cols; i++)
 				System.out.println(source.getColumnName(i) + ": "
 						+ source.getValueAt(row, i));
+
+			beursModel.setSelectedTable(source);
 		}
 	}
 }
