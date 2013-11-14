58377f51ad800400db8ff0aeeea9b88b4686012d
FalkenS <steffalkenburg@gmail.com>
2013-11-14 12:52:26 +0100

    connect en close toegevoegd

--- Internet en netwerken/src/Client/control/ClientConnection.java
+++ Internet en netwerken/src/Client/control/ClientConnection.java
@@ -12,49 +12,60 @@ import Client.model.ClientModel;
 import Client.model.UserModel;
 
 public class ClientConnection extends Thread{
-	private UserModel userModel;
-	private ClientModel clientModel;
 	private Socket connectionSocket;
 	private InputStream in;
-	private 
+	private BufferedReader inFromClient;
+	private DataOutputStream outToClient;
 
 	public ClientConnection(){
+		connect();
+	}
+
+//	public void run() {
+//		try {
+//
+//			//outToClient.writeBytes("GET HTTP/1.1\n\r\n\r");
+//			outToClient.writeBytes("Aandeel\n\r\n\r");
+//
+//			String requestMessageLine;
+//
+//			while (!connectionSocket.isClosed()) {
+//				if (in.available() > 0) {
+//					requestMessageLine = inFromClient.readLine();
+//					System.out.println("Response: "
+//							+ requestMessageLine);
+//				}
+//			}
+//
+//		} catch (Exception ex) {
+//			System.out.println("ClientConnection|Run: " + ex);
+//		}
+//	}
+	
+	public void connect(){
 		try {
 			connectionSocket = new Socket("localhost", 800);
+			in = connectionSocket.getInputStream();
+
+			inFromClient = new BufferedReader(
+					new InputStreamReader(in));
+
+			outToClient = new DataOutputStream(
+					connectionSocket.getOutputStream());
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-
-	public void run() {
+	
+	public void close(){
 		try {
-
-			InputStream in = connectionSocket.getInputStream();
-
-			BufferedReader inFromClient = new BufferedReader(
-					new InputStreamReader(in));
-
-			DataOutputStream outToClient = new DataOutputStream(
-					connectionSocket.getOutputStream());
-
-			//outToClient.writeBytes("GET HTTP/1.1\n\r\n\r");
-			outToClient.writeBytes("Aandeel\n\r\n\r");
-
-			String requestMessageLine;
-
-			while (!connectionSocket.isClosed()) {
-				if (in.available() > 0) {
-					requestMessageLine = inFromClient.readLine();
-					System.out.println("Response: "
-							+ requestMessageLine);
-				}
-			}
-
-		} catch (Exception ex) {
-			System.out.println("ClientConnection|Run: " + ex);
+			connectionSocket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
+	
 }
