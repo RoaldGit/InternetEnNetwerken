ccd38548974940c6cb54417a0d3c1406efcbcebb
FalkenS <steffalkenburg@gmail.com>
2013-11-14 11:17:14 +0100

    connection in aparte class gezet

---
+++ Internet en netwerken/src/Client/control/ClientConnection.java
@@ -0,0 +1,51 @@
+package Client.control;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+import Client.model.ClientModel;
+import Client.model.UserModel;
+
+public class ClientConnection extends Thread{
+	private UserModel userModel;
+	private ClientModel clientModel;
+
+	public ClientConnection(UserModel userModel, ClientModel clientModel){
+		this.userModel = userModel;
+		this.clientModel = clientModel;
+	}
+
+	public void run() {
+		try {
+			Socket connectionSocket = new Socket("localhost", 800);
+
+			InputStream in = connectionSocket.getInputStream();
+
+			BufferedReader inFromClient = new BufferedReader(
+					new InputStreamReader(in));
+
+			DataOutputStream outToClient = new DataOutputStream(
+					connectionSocket.getOutputStream());
+
+			//outToClient.writeBytes("GET HTTP/1.1\n\r\n\r");
+			outToClient.writeBytes("Aandeel\n\r\n\r");
+
+			String requestMessageLine;
+
+			while (!connectionSocket.isClosed()) {
+				if (in.available() > 0) {
+					requestMessageLine = inFromClient.readLine();
+					System.out.println("Response: "
+							+ requestMessageLine);
+				}
+			}
+
+		} catch (Exception ex) {
+			System.out.println("LoginControl|Connecting: " + ex);
+		}
+	}
+
+}
