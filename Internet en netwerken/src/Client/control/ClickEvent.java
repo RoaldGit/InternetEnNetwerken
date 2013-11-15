a715b2a3fd7676f60f401a8bd472e9fc75bc8925
Roald <305953@student.saxion.nl>
2013-11-15 15:10:50 +0100

    Event losgekoppelt van tabel

---
+++ Internet en netwerken/src/Client/control/ClickEvent.java
@@ -0,0 +1,22 @@
+package Client.control;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import Client.model.BeursModel;
+
+public class ClickEvent extends MouseAdapter {
+	private BeursModel beursModel;
+
+	public ClickEvent(BeursModel bModel) {
+		beursModel = bModel;
+	}
+
+	public void mouseClicked(MouseEvent e) {
+		Tabel source = (Tabel) e.getSource();
+		int row = source.getSelectedRow();
+		int cols = source.getColumnCount();
+
+		beursModel.setSelectedTable(source);
+	}
+}
