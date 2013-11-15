f4c9a20f9825fcf6cb39cd0da5c28d3276694693
Roald <305953@student.saxion.nl>
2013-11-15 12:53:09 +0100

    Code iets opgeschoont

--- Internet en netwerken/src/server/WebServer.java
+++ Internet en netwerken/src/server/WebServer.java
@@ -6,13 +6,10 @@ import java.util.*;
 
 public class WebServer extends Thread {
 	private String requestMessageLine;
-	private String fileName;
-	private int port;
 	private ServerSocket listenSocket;
 	private String user= "a", pass="b";
 
 	public WebServer(int port) {
-		this.port = port;
 		try {
 			listenSocket = new ServerSocket(port);
 		} catch (Exception e) {
@@ -47,20 +44,37 @@ public class WebServer extends Thread {
 
 				StringTokenizer tokenizedLine = new StringTokenizer(
 						requestMessageLine);
+
 				//TODO Meerdere filters toevoegen, Data ophalen etc.
 				if (tokenizedLine.hasMoreTokens()) {
 					String firstToken = tokenizedLine.nextToken();
-					if (firstToken.equals("Login")) {
-						if (checkLogin(tokenizedLine)) {
-							outToClient.writeBytes("Login ok\n\r\n\r");
-						} else
-							outToClient.writeBytes("Login oak\n\r\n\r");
-					}
- else if (firstToken.equals("Aan")) {
-						//TODO Aandelen kopen of verkopen
-					}
-					else if (requestMessageLine.contains("Done"))
+					switch (firstToken) {
+					case "Login":
+						checkLogin(tokenizedLine, outToClient);
+						break;
+					case "Aandeel":
+						getPorto(tokenizedLine, outToClient);
+						// TODO Aandelen van de user ophalen
+						// Request bevat username en (waarschijnlijk) soort van
+						// identifier
+						break;
+					case "Koop":
+						koopAandeel(tokenizedLine, outToClient);
+						// TODO Aandeel order plaatsen
+						// Fancy implementatie kijkt ook naar bestaande verkoop
+						// orders
+						break;
+					case "Verkoop":
+						verkoopAandeel(tokenizedLine, outToClient);
+						// TODO Aandeel order plaatsen
+						// Fancy implementatie kijkt ook naar bestaande koop
+						// orders
+						break;
+					case "Done":
+						outToClient.writeBytes("Ack");
 						connectionSocket.close();
+						break;
+					}
 				}
 			}
 		} catch (Exception e) {
@@ -68,36 +82,37 @@ public class WebServer extends Thread {
 		}
 	}
 
-	public boolean checkLogin(StringTokenizer tokenizedLine) {
+	public void checkLogin(StringTokenizer tokenizedLine,
+			DataOutputStream outToClient) {
 		String username, password;
 		username = tokenizedLine.nextToken();
 		password = tokenizedLine.nextToken();
-
-		if (user.equals(username) && pass.equals(password))
-			return true;
-		else
-			return false;
+		try {
+			if (user.equals(username) && pass.equals(password))
+				outToClient.writeBytes("Login ok\n\r\n\r");
+			// TODO DB checken voor login gegevens;
+			else
+				outToClient.writeBytes("Login incorrect\n\r\n\r");
+		} catch (Exception e) {
+			System.out.println("Webserver|checkLogin: " + e);
+		}
 	}
 	
-	public String getAandeel(StringTokenizer tokenizedLine){
-		String aandeel = "";
-		//TODO Huidige data van user ophalen
-		return aandeel;
-	}
-	
-	public String getPorto(StringTokenizer tokenizedLine){
-		String porto = "";
+	public String[][] getPorto(StringTokenizer tokenizedLine,
+			DataOutputStream outToClient) {
 		//TODO Huidige porto ophalen van User
-		return porto;
+		return null;
 	}
 	
-	public boolean sellAandeel(StringTokenizer tokenizedLine){
+	public boolean verkoopAandeel(StringTokenizer tokenizedLine,
+			DataOutputStream outToClient) {
 		boolean verkocht = false;
 		//TODO aandeel verkopen, dingen updaten, boolean returnen
 		return verkocht;
 	}
 	
-	public boolean koopAandeel(StringTokenizer tokenizedLine){
+	public boolean koopAandeel(StringTokenizer tokenizedLine,
+			DataOutputStream outToClient) {
 		boolean gekocht = false;
 		return gekocht;
 	}
